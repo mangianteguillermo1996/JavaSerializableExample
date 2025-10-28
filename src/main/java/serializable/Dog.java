ac807da57219ccfa76fccac6bdda38f9bdc2dcd8
Guillermo Mangiante <guillermo.mangiante@redjar.com.ar>
2025-10-28 14:29:29 -0300

    Agregado de contenido

--- src/main/java/serializable/Dog.java
+++ src/main/java/serializable/Dog.java
@@ -1,4 +1,35 @@
 package serializable;
 
-public class Dog {
+import java.io.Serializable;
+
+public class Dog implements Serializable {
+
+    private String name;
+    private Integer age;
+
+    public Dog(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" + " name = " + name + ", age = " + age + '}';
+    }
 }
--- src/main/java/serializable/Main.java
+++ src/main/java/serializable/Main.java
@@ -1,4 +1,69 @@
 package serializable;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+
+        //CREAR LA LISTA
+        List<Dog> dogList = new ArrayList<>();
+
+        Dog dog = new Dog("Summer", 2);
+        Dog dog2 = new Dog("Susanita", 4);
+        Dog dog3 = new Dog("Mimoso", 1);
+
+        dogList.add(dog);
+        dogList.add(dog2);
+        dogList.add(dog3);
+
+        //GUARDAR LA LISTA
+        FileOutputStream fos = null;
+
+        try {
+
+            fos = new FileOutputStream("Salida.txt");
+
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+            oos.writeObject(dogList);
+
+            oos.close();
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        //RECUPERAR LA LISTA
+
+        List<Dog>  dogList2 = new ArrayList<>();
+        FileInputStream fis = null;
+
+        try {
+
+            fis = new FileInputStream("Salida.txt");
+
+            ObjectInputStream ois = new ObjectInputStream(fis);
+
+            dogList2 = (List<Dog>)ois.readObject();
+
+            ois.close();
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        for (Dog dogs : dogList2) {
+            System.out.println(dogs.toString());
+        }
+
+
+
+
+    }
 }
